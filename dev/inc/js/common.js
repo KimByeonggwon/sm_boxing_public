f8f469f58527aee4ba66dd5e253495e3eb137084
kimBK <wnrhd22@etibe.co.kr>
2019-11-09 15:52:11 +0900

    팝업 관련 수정

--- dev/inc/js/common.js
+++ dev/inc/js/common.js
@@ -402,6 +402,44 @@
 		}
 	}
 
+	$('.btn-pop').click(function(e){
+		e.preventDefault();
+		var $href = $(this).attr('href');
+		layer_popup($href);
+		$('.dimm').fadeIn();
+	});
+
+
+	function layer_popup(el){
+
+			var $el = $(el);        //레이어의 id를 $el 변수에 저장
+			var isDim = $el.prev().hasClass('dimBg');   //dimmed 레이어를 감지하기 위한 boolean 변수
+
+			isDim ? $('.dim-layer').fadeIn() : $el.fadeIn();
+
+			var $elWidth = ~~($el.outerWidth()),
+					$elHeight = ~~($el.outerHeight()),
+					docWidth = $(document).width(),
+					docHeight = $(document).height();
+
+			// 화면의 중앙에 레이어를 띄운다.
+			if ($elHeight < docHeight || $elWidth < docWidth) {
+					$el.css({
+							marginTop: -$elHeight /2,
+							marginLeft: -$elWidth/2
+					})
+			} else {
+					$el.css({top: 0, left: 0});
+			}
+
+			$el.find('a.og-close').click(function(){
+				$el.fadeOut();
+				$('.dimm').fadeOut();
+				return false;
+
+			});
+	}
+
 
 }(jQuery);
 
